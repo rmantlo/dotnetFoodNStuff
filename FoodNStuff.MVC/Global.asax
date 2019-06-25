f129d460ec8dcc1e3ae54d24bfc93b9ad26c8c0f
Becky <you@example.com>
2019-06-25 10:35:05 -0400

    Add project files.

---
+++ FoodNStuff.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodNStuff.MVC.MvcApplication" Language="C#" %>
---
+++ FoodNStuff.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodNStuff.MVC.Startup))]
+namespace FoodNStuff.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
