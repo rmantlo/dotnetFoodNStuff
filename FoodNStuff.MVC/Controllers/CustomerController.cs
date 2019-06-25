fc154891eb9b3c17be504692f5b91cd5440b673d
Becky <you@example.com>
2019-06-25 12:01:11 -0400

    create options started

---
+++ FoodNStuff.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using FoodNStuff.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodNStuff.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+
+        //GET: Customer/Create
+        public ActionResult Create()
+        {
+
+        }
+        //POST: Customer/Create
+        public ActionResult Create()
+        {
+
+        }
+        //GET: Customer/Edit/{id}
+        //POST: Customer/Edit/{id}
+        //GET: Customer/Delete/{id}
+        //POST: Customer/Delete/{id}
+    }
+}
--- FoodNStuff.MVC/Models/Customer.cs
+++ FoodNStuff.MVC/Models/Customer.cs
@@ -12,11 +12,14 @@ namespace FoodNStuff.MVC.Models
         public int CustomerID { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Display(Name = "Full Name")]
         //public string FullName => $"{FirstName} {LastName}"; //this does same thing as below, works only with get only statements
         public string FullName { get { return $"{FirstName} {LastName}"; } }
     }
